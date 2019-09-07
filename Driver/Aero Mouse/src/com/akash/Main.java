7dc2836b0633bc4f87cbb27e14883f69709a0765
Mehdi Hassan Akash <32744268+mhakash@users.noreply.github.com>
2019-09-07 20:45:50 +0600

    update

--- Driver/Aero Mouse/src/com/akash/Main.java
+++ Driver/Aero Mouse/src/com/akash/Main.java
@@ -1,48 +1,72 @@
 package com.akash;
+
 import com.fazecast.jSerialComm.*;
+
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Robot robot = new Robot();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         System.out.println("Height = " + screenSize.height);
         System.out.println("Width = " + screenSize.width);
         double i = 1, j = 1;
-        double posx = 100, posy = 100;
-        double tempx = 0, tempy = 0;
+        int posx = screenSize.width / 2, posy = screenSize.height / 2;
+        double tempx = posx, tempy = posy;
 
-        robot.mouseMove((int)posx, (int)posy);
+        robot.mouseMove(posx, posy);
 
 
-	// write your code here
+        // write your code here
         System.out.println("ok");
         SerialPort ports[] = SerialPort.getCommPorts();
+        //SerialPort port = null;
 
-        for(SerialPort port : ports){
+        for (SerialPort port : ports) {
             System.out.println(port.getSystemPortName());
+//            if(port1.getPortDescription().equalsIgnoreCase("BthModem0")) {
+//                port = port1;
+//            }
         }
 
         SerialPort port = ports[3];
+        //SerialPort port = SerialPort.getCommPort("COM8");
 
-        if(port.openPort()){
+
+        if (port != null && port.openPort()) {
             System.out.println("port opened");
             System.out.println(port.getDescriptivePortName());
-        }else{
+            System.out.println(port.getPortDescription());
+        } else {
             System.out.println("unable to open port");
             return;
         }
 
-        port.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER,0,0);
+        port.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
         Scanner data = new Scanner(port.getInputStream());
 
-        try{
-            while (data.hasNextLine()){
-                String s = data.nextLine();//assuming data comes like "m,100,200" format
-                System.out.println(s);
+        try {
+            while (data.hasNextLine()) {
+                try {
+                    String s = data.nextLine();//assuming data comes like "m,100,200" format
+                    //System.out.println(s);
+                    String[] acc = s.split(",");
+                    /*for(String str: acc) {
+                        System.out.println(str);
+                    }*/
+                    //System.out.print(acc[0]);
+                    double accX = Double.parseDouble(acc[2].trim()) + 0.37;
+                    //System.out.println(" " + accX);
+                    double accY = Double.parseDouble(acc[0].trim()) - 0.75;
+                    System.out.printf("%.3f       %.3f\n", accX, accY);
+                    tempx -= (accX/8);
+                    tempy -= (accY/8);
+                    posx = (int) tempx;
+                    posy = (int) tempy;
+                    robot.mouseMove(posx, posy);
                 /*String[] parts = s.split(",");
                 String mode = parts[0];
                 tempx = (double)Integer.parseInt(parts[1]);
@@ -72,9 +96,13 @@ public class Main {
                     robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
                     robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
                 }*/
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
             }
-        }catch (Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
